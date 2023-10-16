4b2829942deb3dcd040150d88602c40e2fb8ac60
ShiTokuon <k021g1160@g.neec.ac.jp>
2023-10-16 16:11:08 +0900

    15_01画面からカメラの拡大率を操作する

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,36 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CameraController : MonoBehaviour
 {
-
     // ボールオブジェクトへの参照
     [SerializeField]
     GameObject player;
 
-    // ボールオブジェクトとメインカメラの距離
+    // スライダーオブジェクトへの参照
+    [SerializeField]
+    GameObject sliderObject;
+
+    // ズームテキストオブジェクトへの参照
+    [SerializeField]
+    GameObject zoomTextObject;
+
+  // ボールオブジェクトとメインカメラの距離
     Vector3 offset;
 
-    // カメラの拡大率の最小値と最大値
-    const float OffsetMin = 50f;
-    const float OffsetMax = 150f;
+    // スライダーコンポーネントの参照
+    Slider zoomSlider;
+
+    // テキストコンポーネントの参照
+    Text zoomText;
+
+    // 拡大率のテキストで変数以外の文字を設定
+    string zoomTextPrefix = "拡大率 : ";
+    string zoomTextSuffix = "%";
+
+    // 拡大率の最小値と最大値
+    const int OffsetMin = 50;
+    const int OffsetMax = 150;
 
     // カメラ拡大率
     [SerializeField, Range(OffsetMin, OffsetMax)]
-    float magnify = 100f;
+    int magnify = 100;
 
     // Start is called before the first frame update
     void Start()
     {
         // オフセットを計算する
         offset = gameObject.transform.position - player.transform.position;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // 参照を取得
+        zoomSlider = sliderObject.GetComponent<Slider>();
+        zoomText = zoomTextObject.GetComponent<Text>();
     }
 
     void LateUpdate()
@@ -51,10 +67,19 @@ public class CameraController : MonoBehaviour
         float offsetDistance = offset.magnitude;
 
         // offsetDistanceに拡大率を掛けて補正後の距離を計算
-        float magnifiedDistance = offsetDistance * magnify / 100f;
+        float magnifiedDistance = offsetDistance * (200f - magnify) / 100f;
 
         // 規格化されたベクトルと拡大後の距離からオフセットを返す
         Vector3 magnifiedOffset = magnifiedDistance * normalizedOffset;
         return magnifiedOffset;
+    }
+
+    public void OnChangedMagnifyValue()
+    {
+        // スライダーの値を拡大率に反映
+        magnify = (int)zoomSlider.value;
+
+        // ZoomTextに文字列を設定
+        zoomText.text = zoomTextPrefix + magnify.ToString() + zoomTextSuffix;
     }
 }
